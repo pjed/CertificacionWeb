2afa0f415806080e2579d4a9205ae0c0eb43661b
Pedro <espinosaduque@gmail.com>
2021-02-26 18:43:39 +0100

    ejercicios

--- Javascript/ejercicios/vencimientos/vencimientos.js
+++ Javascript/ejercicios/vencimientos/vencimientos.js
@@ -1,89 +1,62 @@
 var fecha;
 var pago;
 var date;
-var dia, mes, anio;
+var dias;
 
 function calculaVencimiento() {
   fecha = document.getElementById("fecha").value;
 
   date = new Date(fecha);
-  dia = date.getDate();
-  mes = date.getMonth() + 1;
-  anio = date.getFullYear();
 
   pago = document.getElementById("pago").value;
 
   //Si es a 30 dias
   if (pago == 0) {
-    /* El mes es enero */
-    if (mes == 1) {
-      //Si comprobar si los dias son 29-30-31
-      if (dia == 29) {
-        dia = 1;
-        mes = 2;
-      } else if (dia == 30) {
-        dia = 2;
-        mes = 2;
-      } else if (dia == 31) {
-        dia = 3;
-        mes = 2;
-      }
-    } else {
-      /* Si los dias son otros */
-      mes = mes + 1;
-    }
-
+    dias = 30;
+    
+    fecha = date.setDate(date.getDate() + dias);
+    date = new Date(fecha);
+    
     /* Escribo la fecha del pago a 30 días */
-    mes = mes + 1;
     document.getElementById("info").innerHTML =
-      "El pago a 30 días acaba el día " + dia + "/" + mes + "/" + anio;
+      "El pago a 30 días acaba el día " +
+      date.getDay() +
+      "/" +
+      (date.getMonth() + 1) +
+      "/" +
+      date.getFullYear();
 
     //Si es a 60 dias
   } else if (pago == 1) {
-    //El mes es diciembre
-    if (mes == 12) {
-      //Comprobar si los dias son 29, 30 o 31
-      if (dia == 29) {
-        dia = 1;
-        mes = 3;
-      } else if (dia == 30) {
-        dia = 2;
-        mes = 3;
-      } else if (dia == 31) {
-        dia = 3;
-        mes = 3;
-      }
-    } else {
-      /* para cualquier otra fecha */
-      mes = 2;
-      anio = anio + 1; 
-    }
-
     /* Escribo la fecha del pago a 60 días */
+    dias = 60;
+
+    
+    fecha = date.setDate(date.getDate() + dias);
+    date = new Date(fecha);
+    
+    /* Escribo la fecha del pago a 30 días */
     document.getElementById("info").innerHTML =
-      "El pago a 60 días acaba el día " + dia + "/" + mes + "/" + anio;
+      "El pago a 60 días acaba el día " +
+      date.getDay() +
+      "/" +
+      (date.getMonth() + 1) +
+      "/" +
+      date.getFullYear();
   } else {
     /* Pago a 90 Días */
-    //El mes es noviembre
-    if (mes == 11) {
-      //Comprobar si los dias son 29, 30 o 31
-      if (dia == 29) {
-        dia = 1;
-        mes = 3;
-      } else if (dia == 30) {
-        dia = 2;
-        mes = 3;
-      } else if (dia == 31) {
-        dia = 3;
-        mes = 3;
-      }
-    } else {
-      /* para cualquier otra fecha */
-      mes = mes + 3;
-    }
+    dias = 90;
 
-    /* Escribo la fecha del pago a 90 días */
+    fecha = date.setDate(date.getDate() + dias);
+    date = new Date(fecha);
+    
+    /* Escribo la fecha del pago a 30 días */
     document.getElementById("info").innerHTML =
-      "El pago a 90 días acaba el día " + dia + "/" + mes + "/" + anio;
+      "El pago a 90 días acaba el día " +
+      date.getDay() +
+      "/" +
+      (date.getMonth() + 1) +
+      "/" +
+      date.getFullYear();
   }
 }
